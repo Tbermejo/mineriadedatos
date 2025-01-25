d12add2c0b5b5ae49551b8c210bd4801d440d4d2
Tbermejo <lbermejo@unal.edu.co>
2025-01-25 12:42:57 -0500

    Update and rename README.md to aplicacion_streamlit.py

---
+++ aplicacion_streamlit.py
@@ -0,0 +1,10 @@
+# mineriadedatos
+import streamlit as st
+def main():
+  st.title("Clasificación de la base de datos mnist")
+  st.markdown("Sube una imagen para clasificar")
+
+if __name__ == "__main__":
+  main()
+
+st.file_uploader
