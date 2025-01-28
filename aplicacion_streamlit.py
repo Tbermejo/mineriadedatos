75039c344fab9694e35b45a58620718935e956b3
Tbermejo <lbermejo@unal.edu.co>
2025-01-28 18:32:51 -0500

    Update aplicacion_streamlit.py

--- aplicacion_streamlit.py
+++ aplicacion_streamlit.py
@@ -1,5 +1,15 @@
 # mineriadedatos
 import streamlit as st
+from PIL import Image
+from tensorflow.keras,preprocessing.image import img_to_array
+
+def preprocess_image(image):
+  image = image.convert('L') #Convertir a escala de grises
+  image = image.resize((28,28))
+  image_array = img_to_array(image) / 255.0
+  image_array = np.expand_dims(image_array, axis=0)
+  return image_array
+  
 def main():
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Sube una imagen para clasificar")
@@ -8,6 +18,10 @@ def main():
   if uploaded_file is not None:
     image = image.open(uploaded_file)
     st.image(image, caption = "Imagen cargada")
+
+    preprocessed_image = preprocess_image(image)
+    st.image(preprocess_image, caption = "Imagen cargada")
+
 if __name__ == "__main__":
   main()
 
