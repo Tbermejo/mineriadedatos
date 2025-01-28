649e2212758a56692b099db0b03453969815ffde
Tbermejo <lbermejo@unal.edu.co>
2025-01-28 18:25:16 -0500

    Update aplicacion_streamlit.py

--- aplicacion_streamlit.py
+++ aplicacion_streamlit.py
@@ -4,9 +4,10 @@ def main():
   st.title("Clasificación de la base de datos mnist")
   st.markdown("Sube una imagen para clasificar")
 
-uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG)", type = ["jpg", "png", "jpeg"])
-
-
+  uploaded_file = st.file_uploader("Selecciona una imagen (PNG, JPG, JPEG)", type = ["jpg", "png", "jpeg"])
+  if uploaded_file is not None:
+    image = image.open(uploaded_file)
+    st.image(image, caption = "Imagen cargada")
 if __name__ == "__main__":
   main()
 
